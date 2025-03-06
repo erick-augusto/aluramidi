bffa25e4d5fa9d72644ed2821b9485a8d93ca6f6
erick-augusto <erick_aug@hotmail.com>
2025-03-06 11:37:30 -0300

    Aprimorando lógica do javascript

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 //enquanto
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     
+    // template string
     const idAudio = `#som_${instrumento}`;
     
-    listaDeTeclas[contador].onclick = listaDeTeclas[contador].onclick = function () {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
