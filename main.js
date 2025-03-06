db6cbcfc4a2f20947174ab7a7ec671fe3b1d0136
erick-augusto <erick_aug@hotmail.com>
2025-03-06 10:34:17 -0300

    Iterando lista de elementos no javascript

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;
+    
+    listaDeTeclas[contador].onclick = listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+}
